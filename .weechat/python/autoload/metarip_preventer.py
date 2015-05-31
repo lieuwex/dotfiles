4ca314ad9855896ccc4561f49c86b6f3d3d5171b
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2015-05-31 18:22:42 +0200

    Add forgotten weechat plugins.

---
+++ .weechat/python/autoload/metarip_preventer.py
@@ -0,0 +1,88 @@
+# coding: utf-8
+#
+# Copyright (c) 2015 by Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com> (http://www.lieuwex.me/)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# 2015-06-31: Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com> (http://www.lieuwex.me/)
+#        0.1: initial release
+
+try:
+    import weechat as w
+except Exception:
+    print("This script must be run under WeeChat.")
+    print("Get WeeChat now at: http://www.weechat.org/")
+    quit()
+
+import re
+
+name = "metarip_preventer"
+author = "Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com> (http://www.lieuwex.me/)"
+version = "0.1"
+license = "GPLv3"
+description = "Prevents some meta-rips."
+shutdown_function = ""
+charset = ""
+
+w.register(name, author, version, license, description, shutdown_function, charset)
+
+metarips = ["ri\np", "irp", "rpi"]
+originalrip = None
+currentrip = None
+ripmessage = None
+
+def on_buffer_input(data, modifier, modifier_data, message):
+    global currentrip, ripmessage, originalrip
+
+    def fixrip(wrongrip, msg=message):
+        return msg.replace(wrongrip, "rip")
+
+    if message.find("/") == 0: # Leave comments alone.
+        return message
+
+    if currentrip is not None:
+        if '\n' in currentrip:
+            splitted = [s.strip() for s in currentrip.split('\n') if len(s) > 0]
+            if len(splitted) > 0:
+                currentrip = '\n'.join(splitted[1:])
+                return ''
+        elif currentrip == message:
+            res = fixrip(originalrip, ripmessage)
+
+            originalrip = None
+            ripmessage = None
+            currentrip = None
+
+            return res
+        else:
+            originalrip = None
+            ripmessage = None
+            currentrip = None
+
+    for word in metarips:
+        if '\n' in word:
+            splitted = [s.strip() for s in word.split('\n') if len(s) > 0]
+            if len(splitted) > 0 and message.split(' ')[-1] == splitted[0]:
+                ripmessage = message
+                originalrip = splitted[0]
+                currentrip = '\n'.join(splitted[1:])
+
+                return ''
+
+        if word in message:
+            return fixrip(word)
+
+    return message
+
+w.hook_modifier('input_text_for_buffer', 'on_buffer_input', '')
